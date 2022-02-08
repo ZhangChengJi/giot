b7a114cc3dab8945335489a13877cae81c43a13a
zcj <11>
2022-02-08 16:45:56 +0800

    增加tdengine

--- pkg/mqtt/mqtt.go
+++ pkg/mqtt/mqtt.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"giot/conf"
 	"giot/pkg/log"
-	"giot/utils"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"time"
 )
@@ -16,7 +15,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 func New(conf *conf.Mqtt) (mqtt.Client, error) {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(fmt.Sprintf("tcp://%s:%v", conf.Host, conf.Port))
-	opts.SetClientID(utils.RandStringBytesRmndr(10))
+	opts.SetClientID(conf.ClientId)
 	opts.SetKeepAlive(5 * time.Second)
 	//opts.SetDefaultPublishHandler(f)
 	opts.SetPingTimeout(1 * time.Second)
